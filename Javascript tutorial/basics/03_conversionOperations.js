92f9b2e210a2fe04fa6f9b43baded7a4bce3b03e
Shantanu kulkarni <140499034+shantanumusic@users.noreply.github.com>
2024-04-25 16:54:01 +0000

    Operators and Conversions

--- Javascript tutorial/basics/03_conversionOperations.js
+++ Javascript tutorial/basics/03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score =  "33abc"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33 easily converted to number
@@ -14,8 +14,8 @@ console.log(valueInNumber);
 
  let isLoggedIn = 1
  let booleanLoggedIn = Boolean(isLoggedIn)
- console.log( typeof (booleanLoggedIn));
- console.log(booleanLoggedIn);
+//  console.log( typeof (booleanLoggedIn));
+//  console.log(booleanLoggedIn);
 
 // 1 => true ; 0 => false
 // "" => false
@@ -24,5 +24,30 @@ console.log(valueInNumber);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
+
+// ******************************* OPERATIONS ******************************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//Arithematic operator +, - , * , ** (power) , / , %
+
+let str1 = "hello"
+let str2 = " shantanu"
+// console.log(str1+str2);
+
+// console.log("1" + 2); // => 12
+// console.log(1 + "2"); // => 12
+// console.log("1" + 2 + 2); // =>122
+// console.log(1 + 2 + "2"); // => 32
+
+console.log(+true);
+console.log(+"")
+
+
+let gameCounter =  100
+gameCounter++
+console.log(gameCounter);
