f017ea0225535659de7c3247788f725c6e028f74
Shantanu kulkarni <140499034+shantanumusic@users.noreply.github.com>
2024-04-25 15:38:49 +0000

    basic of variable and datatype done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "shantanu@gmail.com"
+var accountPassword = "1123344"
+acoountCity = "Chhtrapati Sambhajinagar"
+let accountState;
+// accountId = 3 not allowed
+console.log(accountId);
+accountEmail = "shant@gmail.com"
+accountPassword = "08357"
+acoountCity = "banglore"
+
+console.table([accountEmail, accountId, accountPassword, acoountCity, accountState])
+    
+    /*
+    NEVER USER VAR BECAUSE OF ISSUES IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+    */
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"user strict" // this means "Treat all JS code newer version"
+
+// alert("hello")// we are using node js therefore alert will not work alert only works in browser
+// code readeability is very important
+
+let name = "shantanu"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2^53
+//big int
+// string => ""
+// boolean => true/flase 
+// null => standalone value
+// undefined => 
+// symbol => 
+
+
+
+
+
+// Object
+
+
+console.log(typeof undefined);
+console.log(typeof null); // null is a object
