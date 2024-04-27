a413fd0d6674acc1bcf5253293bbd25edcef3ed6
Shantanu kulkarni <140499034+shantanumusic@users.noreply.github.com>
2024-04-27 19:33:35 +0000

    Arrays part 1

---
+++ Javascript tutorial/02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+// array
+
+const myArr = [0,1,2,3,4,5,"shantanu",true]
+console.log(myArr[6]); // heap memory
+const myHeros = ["shaktiman" , "nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+// Array Methods 
+
+myArr2.push(6)
+console.log(myArr2);
+myArr2.pop()
+
+myArr2.unshift(9)
+console.log(myArr2);
+
+myArr3 = [0,1,2,3,4,5]
+
+console.log(myArr3.includes(3));
+
+const newArr = myArr3.join()
+console.log(myArr3);
+console.log(typeof myArr3);
+
+
+// Major difference between slice and splice is that in slice original
+// array is not manipulated but in splice it is
+
