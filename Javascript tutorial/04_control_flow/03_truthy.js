3c3a4668a38ad550faf4bd6d58c742983f027a8f
Shantanu kulkarni <140499034+shantanumusic@users.noreply.github.com>
2024-05-01 18:51:58 +0000

    conditional flow done

---
+++ Javascript tutorial/04_control_flow/03_truthy.js
@@ -0,0 +1,23 @@
+const userEmail = "h@test.ai"
+
+
+if (userEmail) {
+  console.log("got it");
+} else {
+  console.log("not got");
+}
+
+
+// FALSY VALUES
+// false , 0 , -0 , bigint , "", null , undefined , NaN
+
+
+
+// truthy values 
+// "0" , "false" , " ", [] , {} , 
+
+// NULLISH COALESCING OPERATOR (??) : NULL UNDEFINED
+
+// let val1 = 5 ?? 10
+// let val1 = null ?? 10
+// let val1 = undefined ?? 10
---
+++ Javascript tutorial/04_control_flow/1.js
@@ -0,0 +1,26 @@
+// IF STATEMENT
+// if (condition){ // condition should be true 
+
+// }
+
+const isUserLoggedIn = true 
+const temperature = 41
+
+if (temperature < 50) {
+  console.log("temperature is less than 50");
+  
+}
+
+if (2 === "2") {
+  console.log("executed");
+   
+}
+
+if (condition) {
+  
+} else {
+  
+}
+/* COMPARISON OPERATORS
+< , > , <= , >= , ==, === , != ,*/
+
